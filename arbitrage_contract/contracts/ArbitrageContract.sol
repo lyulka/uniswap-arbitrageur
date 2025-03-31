78074b7c5cce8450ee57a6910159569e80f74bfc
Alice Lim <lexiestrelka@gmail.com>
2025-03-31 22:19:02 +0800

    Write function signatures for the basic functionality of ArbitrageContract.

--- arbitrage_contract/contracts/ArbitrageContract.sol
+++ arbitrage_contract/contracts/ArbitrageContract.sol
@@ -11,4 +11,48 @@ contract ArbitrageContract {
     function hello() external view returns (string memory) {
         return message;
     }
+
+
+    /**
+     * @notice executes a single-path atomic arbitrage involving Uniswap V2 and V3 pairs according to the
+     *         provided `arbitrageRequest`. This is the entrypoint of this contract for external accounts.
+     * @param arbitrageRequest The arbitrage request encoded as a bytes payload.
+     */
+    function arbitrage(bytes calldata arbitrageRequest) external {
+        // Implementation to initiate the flash swap/arbitrage chain goes here.
+    }
+
+    /**
+     * @notice Uniswap V2 flash swap callback. This method should only be called via a cross-contract call
+               from a Uniswap V2 Pair Contract.
+     * @param sender The address initiating the flash swap.
+     * @param amount0 The amount of token0 transferred to the next pair contract in the arbitrage chain.
+     * @param amount1 The amount of token1 transferred to the next pair contract in the arbitrage chain.
+     * @param data The `arbitrageRequest` bytes vector initially passed to the `arbitrage` method.
+     */
+    function uniswapV2Call(
+        address sender,
+        uint amount0,
+        uint amount1,
+        bytes calldata data
+    ) external {
+        // Implementation for processing the Uniswap V2 flash swap callback goes here.
+    }
+
+    /**
+     * @notice Uniswap V3 flash swap callback. This method should only be called via a cross-contract call
+     *         from a Uniswap V3 Pair Contract.
+     * @param fee0 The amount of token0 that we have to pay back to the pair contract at the end of this
+              call.
+     * @param fee1 The amount of token1 that we have to pay back to the pair contract at the end of this
+              call.
+     * @param data The `arbitrageRequest` bytes vector initially passed to the `arbitrage` method.
+     */
+    function uniswapV3FlashCallback(
+        uint256 fee0,
+        uint256 fee1,
+        bytes calldata data
+    ) external {
+        // Implementation for processing the Uniswap V3 flash swap callback goes here.
+    }
 }
