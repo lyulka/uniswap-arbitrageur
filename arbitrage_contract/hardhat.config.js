1f0d7da2a09879a2c45e2d8829ad903027ce06d5
Alice Lim <lexiestrelka@gmail.com>
2025-03-31 19:38:59 +0800

    Update RPC URL to sepolia testnet instead of goerli.

--- arbitrage_contract/hardhat.config.js
+++ arbitrage_contract/hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+        url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       }
     },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     }
   }
